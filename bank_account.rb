fc5212a001f5cad4eb756be7863c0404415437fe
Carmen Chan <carmenkellychan@gmail.com>
2016-08-27 09:06:48 -0700

    Wave 3 completed

--- bank_account.rb
+++ bank_account.rb
@@ -1,61 +1,5 @@
-#August 23rd 2016
-#  WAVE 1 REQUIREMENTS:
-
-# Create a Bank module which will contain your Account class and any future bank account logic.
-#
-# Create an Account class which should have the following functionality:
-#
-# A new account should be created with an ID and an initial balance
-# Should have a withdraw method that accepts a single parameter which represents the amount of money that will be withdrawn. This method should return the updated account balance.
-# Should have a deposit method that accepts a single parameter which represents the amount of money that will be deposited. This method should return the updated account balance.
-# Should be able to access the current balance of an account at any time.
-# Error handling
-#
-# A new account cannot be created with initial negative balance - this will raise an ArgumentError (Google this)
-# The withdraw method does not allow the account to go negative - Will output a warning message and return the original un-modified balance
-
-#OPTIONAL:
-#
-# Create an Owner class which will store information about those who own the Accounts.
-# This should have info like name and address and any other identifying information that an account owner would have.
-# Add an owner property to each Account to track information about who owns the account.
-# The Account can be created with an owner, OR you can create a method that will add the owner after the Account has already been created.
-
-# WAVE 2 REQUIREMENTS:
-
-# Update the Account class to be able to handle all of these fields from the CSV file used as input.
-# For example, manually choose the data from the first line of the CSV file and ensure you can create a new instance of your Account using that data
-# Add the following class methods to your existing Account class
-# self.all - returns a collection of Account instances, representing all of the Accounts described in the CSV. See below for the CSV file specifications
-
-# self.find(id) - returns an instance of Account where the value of the id field in the CSV matches the passed parameter
-# CSV Data File
-#
-# Bank::Account
-#
-# The data, in order in the CSV, consists of:
-# ID - (Fixnum) a unique identifier for that Account
-# Balance - (Fixnum) the account balance amount, in cents (i.e., 150 would be $1.50)
-# OpenDate - (Datetime) when the account was opened
-#
-# OPTIONAL:
-#
-# Implement the optional requirement from Wave 1
-# Add the following class methods to your existing Owner class
-#
-# self.all - returns a collection of Owner instances, representing all of the Owners described in the CSV. See below for the CSV file specifications
-# self.find(id) - returns an instance of Owner where the value of the id field in the CSV matches the passed parameter
-
-# Bank::Owner
-# The data, in order in the CSV, consists of:
-# ID - (Fixnum) a unique identifier for that Owner
-# Last Name - (String) the owner's last name
-# First Name - (String) the owner's first name
-# Street Addess - (String) the owner's street address
-# City - (String) the owner's city
-# State - (String) the owner's state
-#
-# To create the relationship between the accounts and the owners use the account_owners CSV file. The data for this file, in order in the CSV, consists of: Account ID - (Fixnum) a unique identifier corresponding to an account Owner ID - (Fixnum) a unique identifier corresponding to an owner
+#August 25th 2016
+#Bank Account
 
 require 'CSV'
 
@@ -65,9 +9,10 @@ module Bank
   class Account
     attr_accessor :balance, :id, :date
 
-    def initialize(initial_balance, id, date)
-      if initial_balance.to_i < 0
-        raise ArgumentError.new("Initial balance must be greater than or equal to zero")
+    def initialize(initial_balance, id, date, min_init_balance = 0)
+      if initial_balance.to_i < min_init_balance
+        raise ArgumentError.new("Initial
+         balance is less than the minimum allowable initial balance")
       end
 
       @balance = initial_balance
@@ -76,18 +21,18 @@ module Bank
     end
 
 #withdraw function
-    def withdraw(amount)
-      @balance = @balance - amount
-      if @balance < 0
-        puts "Sorry, your balance is less than the withdraw amount."
-        @balance = @balance + amount
+    def withdraw(amount, fee=0, limit = 0)
+      @balance = balance - amount - fee
+      if balance < limit
+        puts "Sorry, your balance is less than the allowable withdraw amount."
+        @balance = balance + amount + fee
       end
-      return @balance
+      return balance
     end
 
 #deposit function
     def deposit(amount)
-      @balance = @balance + amount
+      @balance = balance + amount
       return @balance
     end
 
@@ -100,7 +45,7 @@ module Bank
     end
 
 #class method to return all the accounts
-    def self.all()
+    def self.all
       return @@users
     end
 
@@ -115,6 +60,100 @@ module Bank
     end
   end
 ######### ACCOUNT CLASS ENDS ###########
+
+
+######### SAVINGSACCOUNT CLASS ############
+class SavingsAccount < Account
+  def initialize(initial_balance, id, date, min_init_balanace = 10)
+    super
+  end
+
+  def withdraw(amount, fee = 2, limit = 0)
+      return super
+  end
+
+  def add_interest(rate)
+    @balance = balance * (1 + rate/100)
+    return (balance * rate/100).round(4)
+  end
+
+end
+########## SAVINGSACCOUNT CLASS ENDS ###############
+
+
+############ CHECKINGACCOUNT CLASS #############
+class CheckingAccount < Account
+  attr_reader :num_check
+
+  def initialize(initial_balance, id, date)
+    @num_check = 3
+    super
+  end
+
+  def withdraw(amount, fee = 1, limit = 0)
+    return super
+  end
+
+  def withdraw_using_check(amount, fee = 0, limit = -10)
+    if num_check == 0
+      fee = 2
+    end
+    @num_check -= 1
+
+    return withdraw(amount, fee, limit)
+  end
+
+  def reset_checks
+    @num_check = 3
+  end
+end
+########### CHECKINGACCOUNT CLASS ENDS ###########
+
+
+########### MONEYMARKETACCOUNT CLASS ############
+class MoneyMarketAccount < SavingsAccount
+  attr_reader :num_transactions
+
+  def initialize(initial_balance, id, date, min_init_balance = 10000)
+    @num_transactions = 6
+    super
+  end
+
+  def money_market_withdraw(amount, fee=0, limit = 10000)
+    if balance < limit
+      puts "sorry, withdrawal is not allowed at this point. Please deposit."
+    elsif (balance - amount) < limit && transactions != nil
+      puts "Your money market account balance is now less than the limit."
+      fee = 100
+      return withdraw(amount, fee)
+    else
+      return nil
+    end
+  end
+
+  def deposit(amount)
+    limit = 10000
+    if amount+balance >= limit
+      super
+    else
+      transactions
+      super
+    end
+  end
+
+  def transactions
+    if num_transactions <= 0
+      puts "You are over the transaction limit. Sorry cannot perform your request."
+      return nil
+    end
+    @num_transactions -= 1
+  end
+
+  def reset_transactions
+    @transactions = 6
+  end
+end
+############# MONEYMARKETACCOUNT CLASS ENDS ##############
 
 
 ############# OWNER CLASS #################
@@ -158,13 +197,7 @@ module Bank
     def initialize()
 
     end
-    # @owner_account =[]
-    # CSV.open("support/account_owners.csv", 'r') do |line|
-    #   owner_account_hash = {}
-    #   owner_account_hash.keys = Account.find(line[0])
-    #   owner_account_hash.value = Owner.find(line[1])
-    #   @owner_account << owner_account_hash
-    # end
+
 
   #return the owner with an account id parameter
     def returnOwner(account_id)
@@ -195,11 +228,54 @@ end
 ######### MAIN PROGRAM ##########
 require "awesome_print"
 
-Bank::Account.createAccount
-ap Bank::Account.all
+# Bank::Account.createAccount
+# ap Bank::Account.all
+# ap Bank::Owner.all
 
-ap Bank::Owner.all
+# user1 = Bank::SavingsAccount.new(50, "2345", "1/1/2016")
+# puts user1.withdraw(50)
+# puts user1.deposit(100)
 
+# puts "should not be able to open an account with $5, the minimum is $10\n"
+# user2 = Bank::SavingsAccount.new(5, "23342", "1/2")
 
+# user3 = Bank::SavingsAccount.new(100, "23342", "1/2")
+# puts "the result should be 48."
+# puts user3.withdraw(50)
+# puts "expected: insufficient fund. 48"
+# puts user3.withdraw(48)
+# puts "interest rate"
+# puts user3.add_interest(0.25)
+
+# user4 = Bank::CheckingAccount.new(100, "2345", "1/1/2016")
+# puts "expected: 49"
+# puts user4.withdraw(50)
+# puts "expected: 49. insufficient fund"
+# puts user4.withdraw(50)
+#
+# puts user4.withdraw_using_check(10)
+# puts user4.withdraw_using_check(10)
+# puts user4.withdraw_using_check(10)
+# puts "expected: num_check exceed 3, fee $2. value 7"
+# puts user4.withdraw_using_check(10)
+# user4.reset_checks
+# puts "expected: reset num_check to  3, fee $0. value -9"
+# puts user4.withdraw_using_check(16)
+#
+# user5 = Bank::MoneyMarketAccount.new(10000, "4231", "142")
+# puts "expect: warning that the account is less than the limit. $100 fee. value 4900"
+# puts user5.money_market_withdraw(5000)
+# puts "expect: withdrawal is not allow."
+# puts user5.money_market_withdraw(200)
+# print "num_transactions: "
+# puts user5.num_transactions
+# puts "deposit 500"
+# puts user5.deposit(500)
+# print "num_transactions: "
+# puts user5.num_transactions
+# puts "deposit 10000"
+# puts user5.deposit(10000)
+# print "num_transactions: "
+# puts user5.num_transactions
 
 ######### MAIN PROGRAM ENDS #########
